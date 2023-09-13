a677fc0c39850b0238f7bb80fe3925e55165a18c
wilbervargas <wilber.vargas@udea.edu.co>
2023-09-13 09:23:06 -0500

    usando libreria faker

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { faker } = require('@faker-js/faker');
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,16 +13,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 0',
-      price: '1200',
-    },
-    {
-      name: 'Producto 1',
-      price: '2200',
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 5;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      Image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
 
 app.get('/products/:id', (req, res) => {
